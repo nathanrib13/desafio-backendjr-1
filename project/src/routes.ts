4e3f9ff2765ef35b9df31bf9069af08173392cc5
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2023-06-02 16:12:38 -0300

    fea: auth created

--- project/src/routes.ts
+++ project/src/routes.ts
@@ -1,11 +1,31 @@
-import { Router } from "express";
+import { Request, Router, Response, NextFunction } from "express";
 import multer = require("multer");
 import { clientController } from "./controllers/createClients.controller";
 import readClientsController from "./controllers/readClients.controller";
+import isUserLoggedIn from "./middlewares/isUserLogged.middleware";
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+require("./passport/passport.setup");
 
 const multerConfig = multer();
 const router = Router();
 
 router.post("/clients", multerConfig.single("file"), clientController);
-router.get("/clients", readClientsController);
+
+router.get("/read/clients", isUserLoggedIn, readClientsController);
+router.get("/");
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/read/clients",
+    failureRedirect: "/",
+  })
+);
+
 export { router };
--- project/src/server.ts
+++ project/src/server.ts
@@ -1,9 +1,14 @@
 import "reflect-metadata";
 import cors = require("cors");
 import express = require("express");
+const session = require("express-session");
+const passport = require("passport");
 import { router } from "./routes";
 
 const app = express();
+app.use(session({ secret: "rentcarisamazing" }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(cors());
 app.use(router);
